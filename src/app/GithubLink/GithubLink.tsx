54373c2a6e2a14e6d0e6a24296838e518ed1f206
suhankins <punishedbernadetta@niuitmo.ru>
2024-02-29 19:20:18 +0300

    added a github link

---
+++ src/app/GithubLink/GithubLink.tsx
@@ -0,0 +1,12 @@
+import styles from './GithubLink.module.css';
+
+export function GithubLink() {
+    return (
+        <a
+            className={styles.githubLink}
+            href="https://github.com/suhankins/copy-spotify-song-name"
+        >
+            Github
+        </a>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GithubLink } from './GithubLink/GithubLink';
 import './globals.css';
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {children}
+                <GithubLink />
+            </body>
         </html>
     );
 }
